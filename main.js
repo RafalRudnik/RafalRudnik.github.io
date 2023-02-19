d9161d240ea45ce11fbf773496a397454f36c0e5
Rafal <111435736+RafalRudnik@users.noreply.github.com>
2023-02-19 12:20:32 +0100

    blak-whiteUpdate

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
 // BURGER MENU //
 
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 const burgerMenu = document.querySelector('ul');
 const burgerIcon = document.querySelector('.burger');
-
-burgerIcon.addEventListener('click', function() {
-    burgerMenu.classList.toggle('activeBurger');
-    burgerIcon.classList.toggle('activeBurger');
-});
-
-const switchMode = document.querySelector('.switch-mode')
-
-switchMode.addEventListener('click', function (){
-    body.classList.toggle('dark')
-})
-
-
-
 // NAVIGATION MENU //
 
 const homeLink = document.querySelector('.liHome');
@@ -25,40 +11,55 @@ const knowagleLink = document.querySelector('.liKnowagle');
 const contactLink = document.querySelector('.liContact');
 
 const homeSection = document.getElementById('homeSection');
-const aboutSection = document.getElementById('aboutSection')
-const knowagleSection = document.getElementById('knowagleSection')
-const contactSection = document.getElementById('contactSection')
+const aboutSection = document.getElementById('aboutSection');
+const knowagleSection = document.getElementById('knowagleSection');
+const contactSection = document.getElementById('contactSection');
 
-homeSection.classList.toggle('active')
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
 
-homeLink.addEventListener('click', function (){
-    aboutSection.classList.remove('active')
-    knowagleSection.classList.remove('active')
-    contactSection.classList.remove('active')
-    homeSection.classList.add('active')
-    //homeSection.classList.toggle('active')
-})
+burgerIcon.addEventListener('click', function () {
+	burgerMenu.classList.toggle('activeBurger');
+	burgerIcon.classList.toggle('activeBurger');
+});
 
-aboutLink.addEventListener('click', function (){
-    homeSection.classList.remove('active')
-    knowagleSection.classList.remove('active')
-    contactSection.classList.remove('active')
-    aboutSection.classList.add('active')
-    //aboutSection.classList.toggle('active')
-})
+const switchMode = document.querySelector('.switch-mode');
 
-knowagleLink.addEventListener('click', function(){
-    homeSection.classList.remove('active')
-    aboutSection.classList.remove('active')
-    contactSection.classList.remove('active')
-    knowagleSection.classList.add('active')
-    //knowagleSection.classList.toggle('active')
-})
+switchMode.addEventListener('click', function () {
+	body.classList.toggle('dark');
+	moon.classList.toggle('blackWhite');
+});
 
-contactLink.addEventListener('click', function(){
-    homeSection.classList.remove('active')
-    aboutSection.classList.remove('active')
-    knowagleSection.classList.remove('active')
-    contactSection.classList.add('active')
-    //knowagleSection.classList.toggle('active')
-})
+homeSection.classList.toggle('active');
+
+homeLink.addEventListener('click', function () {
+	aboutSection.classList.remove('active');
+	knowagleSection.classList.remove('active');
+	contactSection.classList.remove('active');
+	homeSection.classList.add('active');
+	//homeSection.classList.toggle('active')
+});
+
+aboutLink.addEventListener('click', function () {
+	homeSection.classList.remove('active');
+	knowagleSection.classList.remove('active');
+	contactSection.classList.remove('active');
+	aboutSection.classList.add('active');
+	//aboutSection.classList.toggle('active')
+});
+
+knowagleLink.addEventListener('click', function () {
+	homeSection.classList.remove('active');
+	aboutSection.classList.remove('active');
+	contactSection.classList.remove('active');
+	knowagleSection.classList.add('active');
+	//knowagleSection.classList.toggle('active')
+});
+
+contactLink.addEventListener('click', function () {
+	homeSection.classList.remove('active');
+	aboutSection.classList.remove('active');
+	knowagleSection.classList.remove('active');
+	contactSection.classList.add('active');
+	//knowagleSection.classList.toggle('active')
+});
