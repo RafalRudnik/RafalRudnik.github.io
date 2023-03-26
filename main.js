5d0602ec74817a83b3f161575ae9e96c66c26b26
Rafal <111435736+RafalRudnik@users.noreply.github.com>
2023-03-26 16:39:47 +0200

    new design

--- main.js
+++ main.js
@@ -1,97 +1,53 @@
-// BURGER MENU //
+const homeBtn = document.querySelector('.home-link');
+const aboutBtn = document.querySelector('.about-link');
+const cntBtn = document.querySelector('.contact-link');
+const portBtn = document.querySelector('.profile-link');
+const allBtns = document.querySelectorAll('.links a');
+const allSections = document.querySelectorAll('.sections');
+const home = document.querySelector('.home-section');
+const about = document.querySelector('.about-section');
+const portfolio = document.querySelector('.portfolio-section');
+const contact = document.querySelector('.contact-section');
 
-const body = document.querySelector('body');
-const burgerMenu = document.querySelector('ul');
-const burgerIcon = document.querySelector('.burger');
+// homeBtn.classList.toggle('active-link');
+// home.classList.toggle('display-on');
 
-const homeLink = document.querySelector('.liHome');
-const aboutLink = document.querySelector('.liAbout');
-const knowagleLink = document.querySelector('.liKnowagle');
-const contactLink = document.querySelector('.liContact');
-
-const homeSection = document.getElementById('homeSection');
-const aboutSection = document.getElementById('aboutSection');
-const knowagleSection = document.getElementById('knowagleSection');
-const contactSection = document.getElementById('contactSection');
-
-const sun = document.querySelector('.sun');
-const moon = document.querySelector('.moon');
-const logo = document.querySelectorAll('.logo');
-
-const sections = document.querySelectorAll('.section');
-
-const switchMode = document.querySelector('.switch-mode');
-
-const sendBtn = document.querySelector('.sendBtn');
-
-const burgerSwitch = () => {
-	burgerMenu.classList.toggle('activeBurger');
-	burgerIcon.classList.toggle('activeBurger');
+const activeLinkRemove = () => {
+	allBtns.forEach((btn) => btn.classList.remove('active-link'));
+};
+const sectionRemove = () => {
+	allSections.forEach((section) => section.classList.remove('display-on'));
 };
 
-const darkMode = () => {
-	body.classList.toggle('dark');
-	moon.classList.toggle('blackWhite');
-	logo.forEach((logo) => logo.classList.toggle('logo-on'));
+const homeBtnActive = () => {
+	activeLinkRemove();
+	sectionRemove();
+	homeBtn.classList.add('active-link');
+	home.classList.add('display-on');
 };
 
-const removeClass = () => {
-	sections.forEach((section) => section.classList.remove('active'));
+const aboutBtnActive = () => {
+	activeLinkRemove();
+	sectionRemove();
+	aboutBtn.classList.add('active-link');
+	about.classList.add('display-on');
 };
-const home = () => {
-	removeClass();
-	homeSection.classList.add('active');
-};
-
-const about = () => {
-	removeClass();
-	aboutSection.classList.add('active');
-};
-
-const knowlage = () => {
-	removeClass();
-	knowagleSection.classList.add('active');
+const portBtnActive = () => {
+	activeLinkRemove();
+	sectionRemove();
+	portBtn.classList.add('active-link');
+	portfolio.classList.add('display-on');
 };
 
-const contact = () => {
-	removeClass();
-	contactSection.classList.add('active');
+const cntBtnActive = () => {
+	activeLinkRemove();
+	sectionRemove();
+	cntBtn.classList.add('active-link');
+	contact.classList.add('display-on');
 };
 
-// EMAIL SENDING  //
+homeBtn.addEventListener('click', homeBtnActive);
+aboutBtn.addEventListener('click', aboutBtnActive);
+portBtn.addEventListener('click', portBtnActive);
+cntBtn.addEventListener('click', cntBtnActive);
 
-function sendMail() {
-	let params = {
-		name: document.getElementById('name').value,
-		email: document.getElementById('email').value,
-		message: document.getElementById('message').value,
-	};
-	const serviceId = 'service_p6oe9zt';
-	const templateId = 'template_nahts8t';
-
-	emailjs
-		.send(serviceId, templateId, params)
-		.then((res) => {
-			document.getElementById('name').value = '';
-			document.getElementById('email').value = '';
-			document.getElementById('message').value = '';
-			console.log(res);
-			alert('message send successfully');
-		})
-		.catch((err) => console.log(err));
-}
-
-// NAVIGATION MENU //
-
-homeSection.classList.toggle('active');
-
-burgerIcon.addEventListener('click', burgerSwitch);
-
-switchMode.addEventListener('click', darkMode);
-
-homeLink.addEventListener('click', home);
-aboutLink.addEventListener('click', about);
-knowagleLink.addEventListener('click', knowlage);
-contactLink.addEventListener('click', contact);
-
-sendBtn.addEventListener('click', sendMail);
